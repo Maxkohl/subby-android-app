1342fd8971bdc7ae9ae89e6e9f076d70ddbe240c
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-13 14:40:40 -0400

    Created getDatabase method that ensures Room is singleton

--- app/src/main/java/com/example/subby/SubsRoomDatabase.java
+++ app/src/main/java/com/example/subby/SubsRoomDatabase.java
@@ -1,14 +1,21 @@
 package com.example.subby;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.DatabaseConfiguration;
 import androidx.room.InvalidationTracker;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
 @Database(entities = {Subscription.class}, version = 1, exportSchema = false)
 public abstract class SubsRoomDatabase extends RoomDatabase {
+    public abstract SubsDao subsDao();
+
+    private static SubsRoomDatabase INSTANCE;
+
     @NonNull
     @Override
     protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
@@ -24,5 +31,18 @@ public abstract class SubsRoomDatabase extends RoomDatabase {
     @Override
     public void clearAllTables() {
 
+    }
+
+    public static SubsRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (SubsRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    //TODO Change migration strategy?
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            SubsRoomDatabase.class, "subs_database").fallbackToDestructiveMigration().build();
+                }
+            }
+        }
+        return INSTANCE;
     }
 }
