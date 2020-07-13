59291536db8fa0d649c23ee606023984a8541fb9
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-13 14:34:47 -0400

    Created SubsRoomDatabase stub

---
+++ app/src/main/java/com/example/subby/SubsRoomDatabase.java
@@ -0,0 +1,28 @@
+package com.example.subby;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+@Database(entities = {Subscription.class}, version = 1, exportSchema = false)
+public abstract class SubsRoomDatabase extends RoomDatabase {
+    @NonNull
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
+
+    @Override
+    public void clearAllTables() {
+
+    }
+}
