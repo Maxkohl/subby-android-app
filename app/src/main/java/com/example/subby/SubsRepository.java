dddb97312e8b63ccf808091aaaa8bcc1e2f5e27a
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-13 14:28:31 -0400

    Added SubsRepository class shell

---
+++ app/src/main/java/com/example/subby/SubsRepository.java
@@ -0,0 +1,13 @@
+package com.example.subby;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class SubsRepository {
+    private SubsDao mSubDao;
+    private LiveData<List<Subscription>> mAllSubs;
+
+
+
+}
