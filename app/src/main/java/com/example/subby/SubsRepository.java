4ba178ca042f5d60d0076d2776b9d1653d039b1e
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-13 14:47:52 -0400

    Added SubDao, getAllSubs method and insertAsyncTask inner class to SubsRepository

--- app/src/main/java/com/example/subby/SubsRepository.java
+++ app/src/main/java/com/example/subby/SubsRepository.java
@@ -1,5 +1,8 @@
 package com.example.subby;
 
+import android.app.Application;
+import android.os.AsyncTask;
+
 import androidx.lifecycle.LiveData;
 
 import java.util.List;
@@ -8,6 +11,33 @@ public class SubsRepository {
     private SubsDao mSubDao;
     private LiveData<List<Subscription>> mAllSubs;
 
+    public SubsRepository(Application application) {
+        SubsRoomDatabase db = SubsRoomDatabase.getDatabase(application);
+        mSubDao = db.subsDao();
+        mAllSubs = mSubDao.getAllSubs();
+    }
+
+    LiveData<List<Subscription>> getAllSubs() {
+        return mAllSubs;
+    }
+
+    public void insert(Subscription sub) {
+        new insertAsyncTask(mSubDao).execute(sub);
+    }
 
 
+    private class insertAsyncTask extends AsyncTask <Subscription, Void, Void>{
+
+        private SubsDao mAsyncTaskDao;
+
+        insertAsyncTask(SubsDao subsDao) {
+            mAsyncTaskDao = subsDao;
+        }
+
+        @Override
+        protected Void doInBackground(Subscription... subscriptions) {
+            mAsyncTaskDao.insert(subscriptions[0]);
+            return null;
+        }
+    }
 }
