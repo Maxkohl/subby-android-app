eed17f158b1de0f28bfce5fccaf936390ad57622
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-13 14:26:34 -0400

    Added table name to Subscription. Created SubsDao with insert and getAllSubs methods

---
+++ app/src/main/java/com/example/subby/SubsDao.java
@@ -0,0 +1,19 @@
+package com.example.subby;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SubsDao {
+
+    @Insert
+    void insert(Subscription sub);
+
+    @Query("SELECT * FROM subs_table ORDER BY name ASC")
+    LiveData<List<Subscription>> getAllSubs();
+
+}
--- app/src/main/java/com/example/subby/Subscription.java
+++ app/src/main/java/com/example/subby/Subscription.java
@@ -5,7 +5,7 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity
+@Entity(tableName = "subs_table")
 public class Subscription {
 
     @NonNull
