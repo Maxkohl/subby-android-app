79215d795b386746ea7e01ac2e1d46cb6a8c5434
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-13 15:14:41 -0400

    Completed SubsListAdapter with inner SubsViewHolder class

--- app/src/main/java/com/example/subby/SubsListAdapter.java
+++ app/src/main/java/com/example/subby/SubsListAdapter.java
@@ -1,6 +1,10 @@
 package com.example.subby;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
@@ -9,25 +13,53 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 import java.util.zip.Inflater;
 
-public class SubsListAdapter extends RecyclerView.Adapter<SubsListAdapter.ViewHolder> {
+public class SubsListAdapter extends RecyclerView.Adapter<SubsListAdapter.SubsViewHolder> {
 
-    private Inflater mInflator;
-    private List<String> mSubs;
+    private LayoutInflater mInflator;
+    private List<Subscription> mSubs;
+
+    SubsListAdapter(Context context) {
+        mInflator = LayoutInflater.from(context);
+    }
 
     @NonNull
     @Override
-    public SubsListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public SubsListAdapter.SubsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflator.inflate(R.layout.recyclerview_item, parent, false);
+        return new SubsViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull SubsListAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull SubsListAdapter.SubsViewHolder holder, int position) {
+        if (mSubs == null) {
+            Subscription current = mSubs.get(position);
+            holder.subItemView.setText(current.getSubName());
+        } else {
+            holder.subItemView.setText("No Subscription");
+        }
 
     }
 
     @Override
     public int getItemCount() {
+        if (mSubs != null) {
+            return mSubs.size();
+        }
         return 0;
+    }
+
+    void setSubs(List<Subscription> subs) {
+        mSubs = subs;
+        notifyDataSetChanged();
+    }
+
+    class SubsViewHolder extends RecyclerView.ViewHolder {
+        private final TextView subItemView;
+
+        private SubsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            subItemView = itemView.findViewById(R.id.subName);
+        }
     }
 
 
