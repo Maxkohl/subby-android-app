272cd10fc63c100aa03ab47020b62faaf1d0e310
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-14 15:48:57 -0400

    Created SubDetailsActivity

---
+++ app/src/main/java/com/example/subby/ui/subs/SubDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.subby.ui.subs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.subby.R;
+
+public class SubDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_details);
+    }
+}
