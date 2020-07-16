0cb25faf3e18b6ec68881f5d4b0ca97a168faed6
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-16 09:48:11 -0400

    Created AlarmReceiver broadcast receiver shell

---
+++ app/src/main/java/com/example/subby/AlarmReceiver.java
@@ -0,0 +1,19 @@
+package com.example.subby;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    private NotificationManager mNotificationManager;
+    private static final int NOTIFICATION_ID = 0;
+
+    
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
